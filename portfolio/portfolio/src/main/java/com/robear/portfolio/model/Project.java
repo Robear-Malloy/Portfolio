68831f48989192d1207990409b44310b4713efbd
Robear-Malloy <robert.malloy0404@gmail.com>
2024-10-01 21:58:34 -0400

    Built Data model for Projects

--- portfolio/portfolio/src/main/java/com/robear/portfolio/model/Project.java
+++ portfolio/portfolio/src/main/java/com/robear/portfolio/model/Project.java
@@ -1,6 +1,5 @@
 package com.robear.portfolio.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,4 +7,66 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "project_id")
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "title")
+    private String title;
+
+    @Basic(optional = false)
+    @Column(name = "description")
+    private String description;
+
+    @Basic(optional = false)
+    @Column(name = "is_featured")
+    private Boolean isFeatured;
+
+    @Column(name = "repo")
+    private String repoLink;
+
+    @Column(name = "demo")
+    private String demoLink;
+
+    @Column(name = "photo")
+    private String photoFile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getIsFeatured() { return isFeatured; }
+
+    public void setIsFeatured(Boolean isFeatured) { this.isFeatured = isFeatured; }
+
+    public String getRepoLink() { return repoLink; }
+
+    public void setRepoLink(String repoLink) { this.repoLink = repoLink; }
+
+    public String getDemoLink() { return demoLink; }
+
+    public void setDemoLink(String demoLink) { this.demoLink = demoLink; }
+
+    public String getPhotoFile() { return photoFile; }
+
+    public void setPhotoFile(String photoFile) { this.photoFile = photoFile; }
 }
