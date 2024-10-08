7ffaad4b690305376a7a7be35460742dea317537
Robear-Malloy <robert.malloy0404@gmail.com>
2024-10-07 23:48:37 -0400

    Setting Up Scaffolding

--- portfolio/portfolio/src/main/java/com/robear/portfolio/service/EducationService.java
+++ portfolio/portfolio/src/main/java/com/robear/portfolio/service/EducationService.java
@@ -6,9 +6,11 @@ import com.robear.portfolio.service.interfaces.IEducationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EducationService implements IEducationService {
 
     private static Logger logger = LoggerFactory.getLogger(EducationService.class);
