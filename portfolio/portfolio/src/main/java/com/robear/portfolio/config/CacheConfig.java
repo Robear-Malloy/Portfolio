03cd01a5acc8a5032654d0c99bb28c689a2c6193
Robear-Malloy <robert.malloy0404@gmail.com>
2025-01-05 00:03:41 -0500

    Added Cache Expiration

--- portfolio/portfolio/src/main/java/com/robear/portfolio/config/CacheConfig.java
+++ portfolio/portfolio/src/main/java/com/robear/portfolio/config/CacheConfig.java
@@ -1,11 +1,13 @@
 package com.robear.portfolio.config;
 
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.annotation.Cacheable;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -13,7 +15,7 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(
                 "skills",
                 "education",
                 "experiences",
@@ -23,5 +25,9 @@ public class CacheConfig {
                 "courses",
                 "techStacks",
                 "videos");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(24, TimeUnit.HOURS)
+                .maximumSize(1000));
+        return cacheManager;
     }
 }
