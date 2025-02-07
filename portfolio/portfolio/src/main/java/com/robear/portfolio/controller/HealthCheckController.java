3975f3dd0e9a622fbe072e8a0a45bb862bc28b0d
Robear-Malloy <robert.malloy0404@gmail.com>
2025-02-07 17:28:26 -0500

    DB Health Check

--- portfolio/portfolio/src/main/java/com/robear/portfolio/controller/HealthCheckController.java
+++ portfolio/portfolio/src/main/java/com/robear/portfolio/controller/HealthCheckController.java
@@ -3,6 +3,7 @@ package com.robear.portfolio.controller;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,28 +19,35 @@ public class HealthCheckController {
     private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
     private final MessageSource messageSource;
+    private final JdbcTemplate jdbcTemplate;
 
-    // Inject MessageSource to access internationalized messages
-    public HealthCheckController(MessageSource messageSource) {
+    public HealthCheckController(MessageSource messageSource, JdbcTemplate jdbcTemplate) {
         this.messageSource = messageSource;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping
     public ResponseEntity<String> healthCheck(@RequestParam(value = "lang", defaultValue = "en") String lang) {
         try {
-            // Add DB Connection Check when set up
-            logger.info("Health Check: App Healthy");
-            logger.info(lang);
-
+            checkDatabaseConnection();
             Locale locale = getValidLocale(lang);
-
             String successMessage = messageSource.getMessage("health.status.healthy", null, locale);
+            logger.info("Health Check: App Healthy");
             return ResponseEntity.ok(successMessage);
         } catch (Exception e) {
-            logger.info("Health Check: App Unhealthy");
-
+            logger.error("Health Check: App Unhealthy - " + e.getMessage(), e);
             String errorMessage = messageSource.getMessage("health.status.unhealthy", new Object[]{e.getMessage()}, Locale.ENGLISH);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+
+    private void checkDatabaseConnection() throws Exception {
+        try {
+            jdbcTemplate.execute("SELECT 1");
+            logger.info("Health Check: Database is healthy.");
+        } catch (Exception e) {
+            logger.error("Health Check: Database connection failed.");
+            throw new Exception("Database connection failed: " + e.getMessage());
         }
     }
 
